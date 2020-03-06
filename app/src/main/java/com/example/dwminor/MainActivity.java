415eedb55813bb8899fd659aefcb7bcbe61d6902
Megha <megha19104@iiitd.ac.in>
2020-03-06 14:54:40 +0530

    hey

--- app/src/main/java/com/example/dwminor/MainActivity.java
+++ app/src/main/java/com/example/dwminor/MainActivity.java
@@ -4,61 +4,116 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity  {
     private Spinner spinner1;
-    Button b;
+    Button login;
     Context context=this;
+    Connection con;
+    EditText username,password;
+    String username1,password1;
+    String un,pass,db,ip;
+    public Connection connectionclass(String user, String password, String database, String server)
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            ConnectionURL="jdbc:jtds:sqlserver://SQL5052.site4now.net;database=DB_A55FDE_dwmini;user=DB_A55FDE_dwmini_admin;password=megha1996";
+            //ConnectionURL = "jdbc:jtds:sqlserver://" + server + database + ";user=" + user+ ";password=" + password + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (SQLException se)
+        {
+            Log.e("error here 1 : ", se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2 : ", e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        Log.d("dekho","Return to dekho kar rha hoon");
+        return connection;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinner1=findViewById(R.id.Signup);
-        List<String> show=new ArrayList<>();
-        show.add(0,"SignUp");
+        System.out.println("dekho ji mujhe na pta kya hua");
+        ip = "SQL5052.site4now.net";
+        db = "DB_A55FDE_dwmini";
+        un = "DB_A55FDE_dwmini_admin";
+        pass = "megha1996";
+        con = connectionclass(un, pass, db, ip);        // Connect to database
+        if (con == null) {
+            String z = "Check Your Internet Access!";
+            System.out.println(z);
+            Log.d("dekho", z);
+        } else {
+            Log.d("dekho", "Connect ho gya khushiya manao");
+        }
+        spinner1 = findViewById(R.id.Signup);
+        List<String> show = new ArrayList<>();
+        show.add(0, "SignUp");
         show.add("Teacher");
         show.add("Student");
         ArrayAdapter<String> arrayAdapter;
-        arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_spinner_item,show);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, show);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(arrayAdapter);
-        b=(Button)findViewById(R.id.loginbutton);
-        b.setOnClickListener(new View.OnClickListener() {
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.loginbutton);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent in=new Intent(context,faculty_profile.class);
-                startActivity(in);
+                username1 = username.getText().toString();
+                password1 = password.getText().toString();
+                check c = new check();
+                c.execute("");
 
             }
         });
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(adapterView.getItemAtPosition(i).equals("SignUp"))
-                {
+                if (adapterView.getItemAtPosition(i).equals("SignUp")) {
                     //Do nothing
-                }
-                else
-                {
-                    String item=adapterView.getItemAtPosition(i).toString();
+                } else {
+                    String item = adapterView.getItemAtPosition(i).toString();
                     //on the basis of selected item do something
-                    if (item=="Teacher")
-                    {
-                        Intent intent=new Intent(MainActivity.this,signup.class);
+                    if (item == "Teacher") {
+                        Intent intent = new Intent(MainActivity.this, signup.class);
                         startActivity(intent);
                     }
-                    if (item=="Student")
-                    {
-                        Intent intent=new Intent(MainActivity.this,signups.class);
+                    if (item == "Student") {
+                        Intent intent = new Intent(MainActivity.this, signups.class);
                         startActivity(intent);
                     }
                 }
@@ -73,6 +128,57 @@ public class MainActivity extends AppCompatActivity  {
 
 
         });
-
     }
-}
+        public class check extends AsyncTask<String,String,String>
+        {
+            String z="";
+            Boolean isSuccess=false;
+
+            @Override
+            protected void onPostExecute(String s) {
+               if(isSuccess){
+                   Toast.makeText(MainActivity.this,"Login successful",Toast.LENGTH_SHORT).show();
+               }
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                if(username1.trim().equals("")|password1.trim().equals("")){
+                    z="please enter username ";
+                }
+
+               else{
+                   try {
+                       {
+                           con = connectionclass(un, pass, db, ip);
+                           if (con == null) {
+                                Log.d("dekho","abhi bhi nahi hua bhai");
+                           } else {
+                               String query = "select * from student_login where s_email = '" + username1.toString() + "' and password = '" + password1.toString() + "' ";
+                               Statement stmt = con.createStatement();
+                               ResultSet rs = stmt.executeQuery(query);
+                               if (rs.next()) {
+                                   z = "Login successful";
+                                   isSuccess = true;
+                                   con.close();
+                               } else {
+                                   z = "Invalid Credentials!";
+                                   isSuccess = false;
+                               }
+                           }
+                       }
+                   } catch (SQLException e) {
+                      e.printStackTrace();
+                  } catch (Exception ex)
+                       {
+                           isSuccess = false;
+                           z = ex.getMessage();
+                       }
+
+                           }
+                return z;
+                       }
+                   }
+
+                }
+
--- app/src/main/java/com/example/dwminor/signups.java
+++ app/src/main/java/com/example/dwminor/signups.java
@@ -5,24 +5,144 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class signups extends AppCompatActivity
 {
-    EditText Password_Register, Confirmpassword, Email_Register;
+    EditText Password_Register, Confirmpassword, Email_Register,sname;
     Button submit, cancel;
+    Boolean isSuccess;
+    String z,name,roll,em,pass,cgpa,com_cr,c_pass;
     Context context=this;
+    public Connection connectionclass()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            ConnectionURL="jdbc:jtds:sqlserver://SQL5052.site4now.net;database=DB_A55FDE_dwmini;user=DB_A55FDE_dwmini_admin;password=megha1996";
+            //ConnectionURL = "jdbc:jtds:sqlserver://" + server + database + ";user=" + user+ ";password=" + password + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (SQLException se)
+        {
+            Log.e("error here 1 : ", se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2 : ", e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        Log.d("dekho","Return to dekho kar rha hoon");
+        return connection;
+    }
+    private void s_details(String Name, String roll,String c,String cg){
+        try {
+            Connection con = connectionclass();
+            if (con == null) {
+                Log.d("dekho", "abhi bhi nahi hua bhai");
+            } else {
+
+                String query = "insert into student values ( '" + roll + "'," + "'" + Name + "'," + "'" + c + "'," + "'" + cg + "')";
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+
+                if (rs.next()) {
+                    z = "Inserted successful";
+                    Toast.makeText(context, z, Toast.LENGTH_SHORT).show();
+                    isSuccess = true;
+                    con.close();
+
+                } else {
+                    z = "Not Inserted!";
+                    Toast.makeText(context, z, Toast.LENGTH_SHORT).show();
+                    isSuccess = false;
+                }
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception ex)
+        {
+            isSuccess = false;
+            z = ex.getMessage();
+        }
+
+    }
+    private void login_details(String roll, String pass,String email){
+        try {
+            Connection con = connectionclass();
+            if (con == null) {
+                Log.d("dekho", "abhi bhi nahi hua bhai");
+            } else {
+
+                String query1 = "insert into student_login values ( '" + email + "'," + "'"+pass + "',"+"'"+roll+"')";
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(query1);
+
+                if (rs.next()) {
+                    z = "Inserted successful";
+                    Toast.makeText(context, z, Toast.LENGTH_SHORT).show();
+                    isSuccess = true;
+                    con.close();
+
+                } else {
+                    z = "Not Inserted!";
+                    Toast.makeText(context, z, Toast.LENGTH_SHORT).show();
+                    isSuccess = false;
+                }
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception ex)
+        {
+            isSuccess = false;
+            z = ex.getMessage();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signups);
-       submit=(Button)findViewById(R.id.Register_submits);
+       submit=(Button)findViewById(R.id.s_submits);
+
+
        submit.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
+               sname = findViewById(R.id.s_name);
+               name = sname.getText().toString();
+               sname = findViewById(R.id.roll_no);
+               roll = sname.getText().toString();
+               sname = findViewById(R.id.Register_emails);
+               em = sname.getText().toString();
+               sname = findViewById(R.id.Register_passwords);
+               pass = sname.getText().toString();
+               sname = findViewById(R.id.c_pass);
+               c_pass = sname.getText().toString();
+               sname = findViewById(R.id.cgpa);
+               cgpa = sname.getText().toString();
+               sname = findViewById(R.id.completed_credit);
+               com_cr = sname.getText().toString();
+               s_details(name,roll,com_cr,cgpa);
+               login_details(roll,pass,em);
                Intent intent=new Intent(context,student_profile.class);
                startActivity(intent);
            }
